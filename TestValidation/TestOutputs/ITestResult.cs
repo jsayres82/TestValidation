a5ecd6b1b3f5362cbe2f1e271ef1beacd6098ac9
Justin Sayres <214782@cubic.com>
2023-06-16 23:08:42 -0400

    checking in ignored files, renamed testresults folder to testoutputs

---
+++ TestValidation/TestOutputs/ITestResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuvo.TestValidation.TestResults
+{
+    public interface ITestResult
+    {
+        string RequirementName { get; set; }
+        bool Passed { get; set; }
+    }
+}
---
+++ TestValidation/TestOutputs/TestResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuvo.TestValidation.TestResults
+{
+    public class TestResult<T> : ITestResult
+    {
+        public string RequirementName { get; set; }
+        public string ParameterName { get; set; }
+        public bool Passed { get; set; }
+        public T ParameterValue { get; set; }
+
+        public TestResult(string parameterName, bool isPassed, T parameterValues)
+        {
+            RequirementName = parameterName;
+            Passed = isPassed;
+            ParameterValue = parameterValues;
+        }
+    }
+}
